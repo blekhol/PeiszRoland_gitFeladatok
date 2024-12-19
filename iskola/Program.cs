45846fc12c0b15243cd301152b3b7e303548a9eb
blekhol <peiszr@gmail.com>
2024-12-19 21:36:06 +0100

    f04 elkészítve

--- iskola/Program.cs
+++ iskola/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Net;
 
 namespace iskola
 {
@@ -10,10 +11,14 @@ namespace iskola
 
         static void Main(string[] args)
         {
-            int[] asd = { 1, 2, 3 , 4, 5};
+            int[] asd = new int[20];
+            for (int i = 0; i < asd.Length; i++)
+            {
+                asd[i] = i;
+            }
             f01(asd);
 
-            //Console.ReadKey();
+            Console.ReadKey();
         }
 
         
@@ -49,6 +54,22 @@ namespace iskola
             atl = atl / har.Length;
 
             return atl;
+        }
+
+        static int[] f04(int[] har)
+        {
+            int[] paratlanok = new int[10];
+
+            for(int i = 0; i < har.Length; i++)
+            {
+                if (har[i] % 2 == 0)
+                {
+                    har[i]++;
+                }
+                paratlanok[i] += har[i];
+            }
+
+            return paratlanok;
         }
     }
 }
